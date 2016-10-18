9d6ae8a456eaa5006fea73579909ef7b0a3f5b73
fermezz <fermezz@gmail.com>
2016-10-18 14:12:18 -0300

    inicio

---
+++ test/tests/suiteISW.java
@@ -0,0 +1,38 @@
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    
+tests.ModuloAritmeticaTest.class
+        
+})
+
+
+public class suiteISW {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    
+}
